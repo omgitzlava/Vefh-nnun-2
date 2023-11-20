a0b1cb926c665b4d39a7b5de87e9873a3b492896
omgitzlava <thelavacrafter53@gmail.com>
2023-11-20 20:15:13 +0000

    added verkefni 3 and verkefni 4

---
+++ css/script.js
@@ -0,0 +1,9 @@
+function toggleNav() {
+    var navbarLinks = document.getElementById("navbarLinks");
+    if (navbarLinks.style.display === "flex") {
+      navbarLinks.style.display = "none";
+    } else {
+      navbarLinks.style.display = "flex";
+    }
+  }
+  
